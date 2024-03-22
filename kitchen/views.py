c83d2c03901c467a7d97bf54f3df5c120d8f5c7a
Maxim Moshenskiy <osidiv@ukr.net>
2024-03-22 17:14:31 +0200

    changed .gitignore and added forms_tests.py

--- kitchen/views.py
+++ kitchen/views.py
@@ -35,7 +35,7 @@ class DishTypeListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(DishTypeListView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
         context["search_form"] = DishTypeSearchForm(
             initial={"name": name}
@@ -73,7 +73,7 @@ class DishListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(DishListView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
         context["search_form"] = DishSearchForm(
             initial={"name": name}
@@ -126,7 +126,7 @@ class CookListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super(CookListView, self).get_context_data(**kwargs)
+        context = super().get_context_data(**kwargs)
         username = self.request.GET.get("username", "")
         context["search_form"] = CookSearchForm(
             initial={"username": username}
