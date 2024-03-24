1fffda764b5d9e5d812a1cef294f1187e5675d51
Maxim Moshenskiy <osidiv@ukr.net>
2024-03-24 14:00:10 +0200

    remove secret key

--- restaurant_kitchen/settings.py
+++ restaurant_kitchen/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-=bt=4nom9^hrx@bt^81(1l(*&6#^ki488kyb1)^z0wq*-wopog")
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 
